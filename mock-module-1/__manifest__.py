620ec8deb0ad18c5e22a7497684197b33428d977
Igor Cvetkovic <igor.cvetkovic@modoolar.com>
2024-10-24 10:12:56 +0000

    Update file dependencies.yml

---
+++ mock-module-1/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "mock module",
+    "summary": "Mock module for split-ci testing",
+    "category": "Project",
+    "version": "18.0.1.0.0",
+    "license": "Other proprietary",
+    "author": "Modoolar",
+    "website": "https://modoolar.com",
+    "external_dependencies": {},
+    "depends": [],
+}
